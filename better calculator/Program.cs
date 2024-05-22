81fe6b3786e86ba814887b1a6ccac879ff86170f
Binato <Binato@DESKTOP-QQO559V>
2024-05-22 20:47:25 -0300

    Adicionar arquivos de projeto.

---
+++ better calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace better_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Coloque um numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Coloque um sinal matemático: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Coloque outro numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else 
+            {
+                Console.WriteLine("Numero invalido");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
